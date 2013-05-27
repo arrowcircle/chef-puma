fda41df3294fa7eb360c8f43c8e2373a6f66ba38
Oleg Bovykin <oleg.bovykin@gmail.com>
2013-05-27 16:45:24 +0400

    fixed template

--- metadata.rb
+++ metadata.rb
@@ -3,3 +3,6 @@ version "0.1.0"
 description "Puma config generator. Part of chef-rails-stack"
 maintainer "Oleg Bovykin"
 maintainer_email "oleg.bovykin@gmail.com"
+
+recipe "puma", "basic configuration for puma"
+recipe "puma::monit", "monit part for puma"
